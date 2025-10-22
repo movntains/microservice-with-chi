f0a669a88cea526aecf3be7fbd0c317112fded50
Rachel Opperman <oppermanre17@gmail.com>
2025-10-22 19:19:42 -0400

    feat: scaffold order routes

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/movntains/microservice-with-chi/application"
 )
 
 func main() {
-	router := chi.NewRouter()
+	app := application.New()
 
-	router.Use(middleware.Logger)
-
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 
 	if err != nil {
-		fmt.Println("Failed to listen to server", err)
+		fmt.Println("Failed to start app:", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world."))
 }
